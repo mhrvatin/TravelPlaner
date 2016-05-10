d6ca3954f55335f11b0a408d70700ad212e844e7
David Andersson <divad1_agaren@hotmail.com>
2016-05-10 12:33:24 +0200

    first commit v.1

--- FlightController.java
+++ FlightController.java
@@ -1,1 +1,63 @@
-123
+/**
+ * 
+ */
+package flightController;
+
+public class flightController {
+	public flightController(){
+		
+	}
+	
+	public boolean bookFlight(int id, int nrOfPassengers){
+		return true;
+
+	}
+	
+	public String[][] getFlights(String origin, String destination, String returnDate, String date){
+		//String[][] allFlights = new String[x][y];
+				String[][] sa1 = new String[4][5];
+		for(int i = 0; i < sa1.length; i++) {           // sa1.length == 4
+		    for (int j = 0; j < sa1[i].length; j++) {     //sa1[i].length == 5
+		        sa1[i][j] = "new String value";
+		    }
+		}
+		
+		for(String item[] : sa1){
+			System.out.println(item[0]);
+		}
+		return sa1;
+		
+	}
+	
+	public String[] getFlightsInfo(int id){
+		String[] x = null;
+		return x;
+	}
+	
+	public boolean addFlight(String origin, String destination, String deptDate, String travelTime, int price,int seats){
+		return false;
+	}
+	
+	public boolean removeFlight(int id){
+		return false;
+	}
+	
+	public boolean updateFlight(String origin, String destination, String deptDate, String travelTime, int price,int seats){
+		return false;
+	}
+	
+	@SuppressWarnings("unused")
+	private boolean addToDatabase(String origin, String destination, String deptDate, String travelTime, int price,int seats){
+		return false;
+	}
+	
+	@SuppressWarnings("unused")
+	private boolean deleteFromDatabase(int id){
+		return false;
+	}
+	
+	@SuppressWarnings("unused")
+	private boolean updateDatabase(String origin, String destination, String deptDate, String travelTime, int price,int seats){
+		return false;
+	}
+}
---
+++ testFlightController.java
@@ -0,0 +1,12 @@
+package flightController;
+
+public class testFlightController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		flightController flightTest = new flightController();
+		System.out.print(flightTest.getFlights("x","x","x","x"));
+	
+	}
+
+}
