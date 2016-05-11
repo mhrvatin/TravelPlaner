cae855afe92e22643c8bba7e08be99a2fb30a07b
David Andersson <divad1_agaren@hotmail.com>
2016-05-11 20:03:35 +0200

    Functions for flights set

--- FlightController.java
+++ FlightController.java
@@ -9,41 +9,36 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
+
 
 public class FlightController {
 	
-	
-	
-	private void dbConnection(String SQL){
+	private boolean dbDelete(String SQL){
 		Connection connection = null;
-
+		boolean success = true;
 		try {
-		    String dbPath = "C:/cygwin64/home/david/projekt/travelplanner/src/travelplanner/pa1415_group.e2_travelplanner.db";
+		    //String dbPath = "C:/cygwin64/home/david/projekt/travelplanner/src/travelplanner/pa1415_group.e2_travelplanner.db";
+		    String dbPath = "C:/cygwin/home/David/projekt/travelplanner/src/travelplanner/pa1415_group.e2_travelplanner.db";
 		    connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
 		    Statement statement = connection.createStatement();
-		    statement.setQueryTimeout(30);  // set timeout to 30 sec.
-		    ResultSet rs = statement.executeQuery("SELECT * FROM flights");
+		    statement.setQueryTimeout(30);  
+		    
 		    if(SQL == ""){
 		    	
 		    }else{
 		    	statement.executeUpdate(SQL);
 		    }
-		    
+		    ResultSet rs = statement.executeQuery("SELECT * FROM flights");
 
 		    while(rs.next()) {
 		        // read the result set
-		        System.out.println("user_id = " + rs.getString("origin"));
-		        /*System.out.println("user_name = " + rs.getString("user_name"));
-		        System.out.println("first_name = " + rs.getString("first_name"));
-		        System.out.println("last_name = " + rs.getString("last_name"));
-		        System.out.println("email = " + rs.getString("email"));
-		        System.out.println("user_password_hash = " + rs.getString("user_password_hash"));*/
+		    	
 		    }
 		} catch(SQLException e) {
 		    // if the error message is "out of memory",
 		    // it probably means no database file is found
 		    System.err.println(e.getMessage());
+		    success = false;
 		} finally {
 		    try {
 		        if(connection != null)
@@ -53,45 +48,148 @@ public class FlightController {
 		        System.err.println(e);
 		    }
 		}
+		return success;
+		
 	}
 	
+	private boolean dbInsert(String SQL){
+		Connection connection = null;
+		boolean success = true;
+		try {
+		    //String dbPath = "C:/cygwin64/home/david/projekt/travelplanner/src/travelplanner/pa1415_group.e2_travelplanner.db";
+		    String dbPath = "C:/cygwin/home/David/projekt/travelplanner/src/travelplanner/pa1415_group.e2_travelplanner.db";
+		    connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+		    Statement statement = connection.createStatement();
+		    statement.setQueryTimeout(30);  
+		    
+		    if(SQL == ""){
+		    	
+		    }else{
+		    	statement.executeUpdate(SQL);
+		    }
+		    ResultSet rs = statement.executeQuery("SELECT * FROM flights");
+
+		    while(rs.next()) {
+		        // read the result set
+		    	
+		    }
+		} catch(SQLException e) {
+		    // if the error message is "out of memory",
+		    // it probably means no database file is found
+		    System.err.println(e.getMessage());
+		    success = false;
+		} finally {
+		    try {
+		        if(connection != null)
+		        connection.close();
+		    } catch(SQLException e) {
+		        // connection close failed.
+		        System.err.println(e);
+		    }
+		}
+		return success;
+		
+	}
+	
+	private Integer[] dbGet(String SQL){
+		Connection connection = null;
+		Integer flight[] = new Integer[2];
+		if(SQL != ""){
+			
+			try {
+			    //String dbPath = "C:/cygwin64/home/david/projekt/travelplanner/src/travelplanner/pa1415_group.e2_travelplanner.db";
+			    String dbPath = "C:/cygwin/home/David/projekt/travelplanner/src/travelplanner/pa1415_group.e2_travelplanner.db";
+			    connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+			    Statement statement = connection.createStatement();
+			    statement.setQueryTimeout(30);  
+			    ResultSet rs = statement.executeQuery(SQL);    
+			    
+			while(rs.next()) {
+			        // read the result set
+			        System.out.println("flight_id = " + rs.getString("flight_id"));
+			        System.out.println("seats = " + rs.getString("nr_of_seats"));
+			        flight[0] = rs.getInt("flight_id");
+			        flight[1] = rs.getInt("nr_of_seats");
+			    }
+			} catch(SQLException e) {
+			    // if the error message is "out of memory",
+			    // it probably means no database file is found
+			    System.err.println(e.getMessage());
+			} finally {
+				if (connection != null) try { connection.close(); } catch (SQLException ignore) {}
+			}
+		}
+		return flight;
+	}
+	
+	private String[][] dbGetFlights(String SQL){
+		Connection connection = null;
+		String flight[][] = null;
+		if(SQL != ""){
+			
+			try {
+			    //String dbPath = "C:/cygwin64/home/david/projekt/travelplanner/src/travelplanner/pa1415_group.e2_travelplanner.db";
+			    String dbPath = "C:/cygwin/home/David/projekt/travelplanner/src/travelplanner/pa1415_group.e2_travelplanner.db";
+			    connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+			    Statement statement = connection.createStatement();
+			    statement.setQueryTimeout(30);  
+			    ResultSet rs = statement.executeQuery(SQL);   
+			    flight = new String[100][8];
+			    int count = 0;
+			while(rs.next()) {
+			        // read the result set
+			       flight[count][0] = String.valueOf(rs.getInt("flight_id"));
+			       flight[count][1] = String.valueOf(rs.getString("origin"));
+			       flight[count][2] = String.valueOf(rs.getString("destination"));
+			       flight[count][3] = String.valueOf(rs.getString("departure_date"));
+			       flight[count][4] = String.valueOf(rs.getString("departure_time"));
+			       flight[count][5] = String.valueOf(rs.getString("travel_time"));
+			       flight[count][6] = String.valueOf(rs.getInt("price"));
+			       flight[count][7] = String.valueOf(rs.getInt("nr_of_seats"));
+					
+			       count++;
+			    }
+			} catch(SQLException e) {
+			    // if the error message is "out of memory",
+			    // it probably means no database file is found
+			    System.err.println(e.getMessage());
+			} finally {
+				if (connection != null) try { connection.close(); } catch (SQLException ignore) {}
+			}
+		}
+		return flight;
+	}
+
 	public FlightController(){
-		this.dbConnection("");
+		//this.dbInsert("");
 	}
 	
 	public boolean bookFlight(int id, int nrOfPassengers){
-		return true;
+		
+		Integer[] flight = this.dbGet("SELECT * FROM flights WHERE flight_id = "+id);
+		int nrOfSeats = flight[1] - nrOfPassengers;
+		
+		return this.dbInsert("UPDATE FLIGHTS SET nr_of_seats = "+nrOfSeats+" WHERE flight_id = "+flight[0]);
 
 	}
 	
 	public String[][] getFlights(String origin, String destination, String returnDate, String date){
-		
-		
-		String[] flight = {"origin","dst","returnD","date"}; //EXTRACT FROM DATABASE, BASED ON PARAMETERS
-		String[][] allFlights = new String[4][4];
-		for(int i = 0; i < allFlights.length; i++) {
-		    for (int j = 0; j < allFlights[i].length; j++) {     
-		        allFlights[i][j] = flight[j];
-		    }
-		}
-		
-		//System.out.println(Arrays.deepToString(allFlights));
-		
-		
-		return allFlights;
+		String[][] flights = null;
+		flights = this.dbGetFlights("SELECT * FROM flights WHERE origin = '"+ origin + "' AND destination = '" + destination + "' AND departure_date = '" + date + "'" );
+		System.out.println(flights[0][1]);
+		return flights;
 		
 	}
 	
 	public String[] getFlightsInfo(int id){
-		//EXTRACT FROM DATABASE, BASED ON PARAMETERS
-		String[] flight = {"origin","dst","returnD","date"};
-		return flight;
+		String[][] flights = null;
+		flights = this.dbGetFlights("SELECT * FROM flights WHERE flight_id = "+id );
+		System.out.println(flights[0][1]);
+		return flights[0];	
 	}
 	
-	public boolean addFlight(String origin, String destination, String deptDate, String travelTime, int price,int seats){
-		
-		
-		return this.addToDatabase(origin, destination,"deptTime", deptDate, travelTime, price, seats);
+	public boolean addFlight(String origin, String destination, String deptDate,String deptTime, String travelTime, int price,int seats){
+		return this.addToDatabase(origin, destination,deptDate, deptTime, travelTime, price, seats);
 		
 	}
 	
@@ -99,26 +197,39 @@ public class FlightController {
 		return this.deleteFromDatabase(id);
 	}
 	
-	public boolean updateFlight(String origin, String destination, String deptDate, String travelTime, int price,int seats){
-		return this.updateDatabase(origin, destination, deptDate, travelTime, price, seats);
+	public boolean updateFlight(int id,String origin, String destination, String deptDate,String deptTime, String travelTime, int price,int seats){
+		return this.updateDatabase(id,origin, destination,deptDate, deptTime, travelTime, price, seats);
 	}
 	
-
-	private boolean addToDatabase(String origin, String destination,String deptTime, String deptDate, String travelTime, int price,int seats){
-		//INSERT INTO DATABASE
-		this.dbConnection("INSERT INTO flights VALUES(2,'Malmo','Karlskrona','2016-06-10','15:02','02:00',3000,240)");
-		return true;
+	private boolean addToDatabase(String origin, String destination, String deptDate,String deptTime, String travelTime, int price,int seats){
+		
+		return this.dbInsert("INSERT INTO flights VALUES( " +
+		    4 + ", " +
+		    "'" + origin + "', " +
+		    "'" + destination + "', " +
+		    "'" + deptDate + "', " +
+		    "'" + deptTime + "', " +
+		    "'" + travelTime + "', " +
+		    price +", " +
+		    seats +
+		    " )");  
+		
 	}
-	
 	
 	private boolean deleteFromDatabase(int id){
-		//DELETE FROM DATABASE
-		return true;
+		return this.dbDelete("DELETE FROM flights WHERE flight_id = "+ id);
 	}
 	
-	
-	private boolean updateDatabase(String origin, String destination, String deptDate, String travelTime, int price,int seats){
-		//UPDATE DATABASE
-		return true;
+	private boolean updateDatabase(int id,String origin, String destination, String deptDate,String deptTime, String travelTime, int price,int seats){
+		return this.dbInsert("UPDATE flights SET " +
+				"origin = '" + origin + 
+				"', destination = '" + destination + 
+				"', departure_date = '" + deptDate +
+				"', departure_time = '" + deptTime +
+				"', travel_time = '" + travelTime +
+				"', price = " + price +
+				",  nr_of_seats = " + seats + 
+				" WHERE flight_id = " + id);
 	}
 }
+
--- testFlightController.java
+++ testFlightController.java
@@ -1,9 +1,5 @@
 package travelplanner;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 
 
 public class testFlightController {
@@ -11,12 +7,13 @@ public class testFlightController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FlightController flightTest = new FlightController();
-		System.out.println("Book Flight - " + flightTest.bookFlight(0, 0));
-		System.out.println("Get Flight - " + flightTest.getFlights("x","x","x","x"));
-		System.out.println("Get Flight Info - " + flightTest.getFlightsInfo(0));
-		System.out.println("Add Flight - " + flightTest.addFlight("x", "x", "x", "x", 0, 0));
-		System.out.println("Remove Flight - " + flightTest.removeFlight(0));
-		System.out.println("Update Flight - " + flightTest.updateFlight("x", "x", "x", "x", 0, 0));
+		//System.out.println("Book Flight - " + flightTest.bookFlight(1, 1)); CHECK
+		//System.out.println("Get Flight - " + flightTest.getFlights("Malmo","Karlskrona","x","2016-06-10")); CHECK
+		//System.out.println("Get Flight Info - " + flightTest.getFlightsInfo(1)); CHECK
+		//System.out.println("Add Flight - " + flightTest.addFlight("x", "x", "2016-06-10", "15:02","02:00", 0, 0)); CHECK
+		//System.out.println("Remove Flight - " + flightTest.removeFlight(1)); CHECK
+		//System.out.println("Update Flight - " + flightTest.updateFlight(3,"TEST2", "TEST2", "2016-06-10", "15:02","02:00", 1337, 1337)); //CHECK
+		
 
 		
 	}
