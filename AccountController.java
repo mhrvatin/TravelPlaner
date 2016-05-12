9bf4546e88b01132805f739b37ba7b6d38cbf978
Timothy Timrin <timothy96@live.se>
2016-05-12 15:08:35 +0200

    AccountController updated
    Login function for db communication, code added (still not done).
    Minor changes to testFlightController, added code to test
    AccountController

--- AccountController.java
+++ AccountController.java
@@ -1,5 +1,10 @@
 package travelplanner;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class AccountController {	
@@ -14,7 +19,97 @@ public class AccountController {
 	
 	public String login()
 	{
-		return "tmp";
+		String ret = "";
+		
+		
+		Connection connection = null;
+		
+		try {
+		    String dbPath = "C:/Users/Administratör/workspace/GIT_JAVA/TravelPlaner/pa1415_group.e2_travelplanner.db";
+		    connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+		    Statement statement = connection.createStatement();
+		    statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+		    ResultSet rs = statement.executeQuery("SELECT first_name, last_name,"
+		    		+ " email, user_password_hash FROM users WHERE email = '"
+		    		+ this.user + "'");
+		    
+		        // read the result set
+		        /*
+		        System.out.println("first_name = " + rs.getString("first_name"));
+		        System.out.println("last_name = " + rs.getString("last_name"));
+		        System.out.println("email = " + rs.getString("email"));
+		        System.out.println("user_password_hash = " + rs.getString("user_password_hash"));
+		        */
+		        
+		        //LOGIN
+		        String pwdHash = rs.getString("user_password_hash");
+		        if(pwdHash.equals(password))
+		        {
+		        	ret = this.user;
+		        	String fullName = (rs.getString("first_name") + " " + rs.getString("last_name"));
+		        }
+		        
+		} catch(SQLException e) {
+		    // if the error message is "out of memory",
+		    // it probably means no database file is found
+		    System.err.println(e.getMessage());
+		} finally {
+		    try {
+		        if(connection != null)
+		        connection.close();
+		    } catch(SQLException e) {
+		        // connection close failed.
+		        System.err.println(e);
+		    }
+		}
+		
+		return ret;
+	}
+	
+	public void testDB()
+	{
+		
+		Connection connection = null;
+
+		try {
+		    String dbPath = "C:/Users/Administratör/workspace/GIT_JAVA/TravelPlaner/pa1415_group.e2_travelplanner.db";
+		    connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+		    Statement statement = connection.createStatement();
+		    statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+		    ResultSet rs = statement.executeQuery("SELECT first_name, last_name,"
+		    		+ " email, user_password_hash FROM users WHERE email = '"
+		    		+ this.user + "'");
+		    
+		        // read the result set
+		        System.out.println("first_name = " + rs.getString("first_name"));
+		        System.out.println("last_name = " + rs.getString("last_name"));
+		        System.out.println("email = " + rs.getString("email"));
+		        System.out.println("user_password_hash = " + rs.getString("user_password_hash"));
+		        
+		        //LOGIN
+		        String pwdHash = rs.getString("user_password_hash");
+		        if(pwdHash.equals(password))
+		        {
+		        	String ret = this.user;
+		        	String fullName = (rs.getString("first_name") + " " + rs.getString("last_name"));
+		        }
+		        
+		} catch(SQLException e) {
+		    // if the error message is "out of memory",
+		    // it probably means no database file is found
+		    System.err.println(e.getMessage());
+		} finally {
+		    try {
+		        if(connection != null)
+		        connection.close();
+		    } catch(SQLException e) {
+		        // connection close failed.
+		        System.err.println(e);
+		    }
+		}
+		
 	}
 	
 	public boolean register(String firstName, String lastName)
@@ -29,6 +124,8 @@ public class AccountController {
 	{
 		String user = this.user;
 		String password = this.password;
+		
+		
 		
 		return true;
 	}
